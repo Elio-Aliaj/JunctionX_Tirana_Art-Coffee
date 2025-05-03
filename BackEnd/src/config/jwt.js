fb6550694dafe84ab99eb9380d1493ea6e5fc497
Klesti03 <klestisylajkle@gmail.com>
2025-05-03 10:39:18 +0200

    .

--- BackEnd/src/config/jwt.js
+++
@@ -1,2 +0,0 @@
-export const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key"
-export const JWT_EXPIRES_IN = "24h"
--- BackEnd/src/index.js
+++
@@ -1,3 +0,0 @@
-// The app is already started in src/index.js, so this file just imports it
-// This allows you to run the app with: node index.js
-import "./src/index.js"

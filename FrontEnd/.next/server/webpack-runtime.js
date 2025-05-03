edd179cd0b807a644f903d485802d8e710ee4678
Savina7 <savinashimi2005@outlook.com>
2025-05-03 17:51:22 +0200

    fix

--- FrontEnd/.next/server/webpack-runtime.js
+++ FrontEnd/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("b6da66f6817d8624")
+/******/ 		__webpack_require__.h = () => ("cea7775f4724d301")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -163,11 +163,6 @@
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
 /******/ 		}
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	(() => {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
